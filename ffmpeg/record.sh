347db041d257de4d256bfea925144c52772476f1
Zeeshan Khan <zkhan1093@gmail.com>
2022-01-13 14:29:28 +0530

    folder check before mkdir, added buffer in timeout, reduced log lvel

--- ffmpeg/record.sh
+++ ffmpeg/record.sh
@@ -4,12 +4,15 @@
 #RTSP_URL=$3
 #FORMAT=$4
 #SEGMENT_FORMAT=$5
-
-mkdir /out/$NAME
+#TIMEOUT_BUFFER=$6
+if [[ ! -e /out/$NAME ]]; then
+  mkdir /out/$NAME
+fi
 chmod 775 /out/$NAME
-
-timeout $DURATION \
-ffmpeg -y -i $RTSP_URL -stimeout 60000000 \
+TIMEOUT=$(( $DURATION + $TIMEOUT_BUFFER ))
+echo timeout $TIMEOUT \
+ffmpeg  -loglevel $LOG_LEVEL \
+-y -i $RTSP_URL -stimeout 60000000 \
 -vsync 1 -async -1 -an -vcodec copy \
 -f segment -strftime 1 -segment_time $DURATION -segment_format $FORMAT "/out/${NAME}/${NAME}_${SEGMENT_FORMAT}.$FORMAT" -reset_timestamps 1 -segment_atclocktime 1
 
