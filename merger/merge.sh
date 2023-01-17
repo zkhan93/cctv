91f3e1c9609f630526886de677bce9cbe517ba5c
Zeeshan Khan <zkhan1093@gmail.com>
2023-01-17 16:58:25 -0500

    skip 720p conversion

--- merger/merge.sh
+++ merger/merge.sh
@@ -30,10 +30,10 @@ cat $OUT
 LINES=$(wc -l < $OUT)
 if [ $LINES -ge "2" ]; then
   echo "merging videos"
-  ffmpeg -loglevel error -f concat -safe 0 -i $OUT -c copy $MERGED_TMP
-  echo "videos merged to $MERGED_TMP"
+  ffmpeg -loglevel error -f concat -safe 0 -i $OUT -c copy $MERGED
+  echo "videos merged to $MERGED"
 
-  if [ $? -eq 0 ] && [ -f $MERGED_TMP ] ; then
+  if [ $? -eq 0 ] && [ -f $MERGED ] ; then
     echo "deleting segment videos"
     for f in $(cat $DELETE) ; do 
       rm "$f"
@@ -47,10 +47,10 @@ if [ -f $DELETE ]; then
   rm $DELETE
 fi
 
-echo "convert to 720p"
-ffmpeg -loglevel error -i $MERGED_TMP -filter:v "scale=-1:720" $MERGED
-if [ $? -eq 0 ] && [ -f $MERGED ] ; then
-  echo "vide converted to 720 $MERGED"
-  echo "deleting original video"
-  rm "$MERGED_TMP"
-fi
+#echo "convert to 720p"
+#ffmpeg -loglevel error -i $MERGED_TMP -filter:v "scale=-1:720" $MERGED
+#if [ $? -eq 0 ] && [ -f $MERGED ] ; then
+#  echo "vide converted to 720 $MERGED"
+#  echo "deleting original video"
+#  rm "$MERGED_TMP"
+#fi
