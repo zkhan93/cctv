df0ad29e5e3de1ec8ca54338d3cdb1396cf55eb7
Zeeshan Khan <zkhan1093@gmail.com>
2023-02-02 16:25:22 -0500

    cleanup fix

--- merger/cleanup.sh
+++ merger/cleanup.sh
@@ -4,7 +4,6 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib/
 DIR=$CLEANUP_DIR
 LIMIT=$CLEANUP_SPACE
 DEL_PATTERN=$CLEANUP_REGEX
-SORT_KEY_AT=$CLEANUP_SORT_KEY
 DELETED=0
 echo "Running on $(date)"
 
@@ -18,7 +17,7 @@ AVAIL_GB=$(($AVAIL/1024/1024))
 echo "${AVAIL_GB} GB Available"
 while [ $AVAIL_GB -lt $LIMIT ]; do
   echo "$LIMIT GB needed, need cleanup"
-  FILE=$(find $DIR -type f -regex $DEL_PATTERN | sort -t / -k $SORT_KEY_AT | head -n 1)
+  FILE=$(find $DIR -type f -regex $DEL_PATTERN | -printf "%T+ %p\n" | sort | cut -c 32- | head -n 1)
   echo "removing $FILE"
   echo "$(du -h $FILE)"
   rm $FILE
